9ee5b1f30b4c3f9ec24bf6215f0409c34e5cdd3b
selmangar5-byte <selmangar5@gmail.com>
2026-02-06 23:58:56 +0000

    🐛 Fix: tenant_scope should return Incident not AnalysisResult

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,12 @@
 # frozen_string_literal: true
-
 class ApplicationController < ActionController::API
   private
-
   def current_tenant_id
     tenant_id = request.headers['X-Tenant-ID'].presence
     raise ActionController::BadRequest unless tenant_id
     tenant_id
   end
-
   def tenant_scope
-    AnalysisResult.where(tenant_id: current_tenant_id)
+    Incident.where(tenant_id: current_tenant_id)
   end
 end
